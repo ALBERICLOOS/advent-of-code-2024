55d6c47024507038e16cda4b509923427b7328a0
ALBERICLOOS <albericloos2003@gmail.com>
2024-12-05 21:38:14 +0100

    part 2 completed

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,8 @@
+import copy
+
+
 def part1():
-    f = open("input.txt", "r")
+    f = open("test.txt", "r")
 
     before = dict() # stores values that need to be before key
 
@@ -35,4 +38,67 @@ def part1():
     print(result)
 
 
-part1()
+def make_correct(update, before):
+
+    result = []
+
+    # only store items in dict that are also in update
+    rules = {}
+    for value in update:
+        if value in before:
+            rules[value] = []
+            for item in before[value]:
+                if item in update:
+                    rules[value].append(item)
+        else:
+            rules[value] = []
+
+    while len(update) > 0:
+        for key in rules:
+            if str(key) in update and rules[key] == []:
+                result.append(key)
+                update.remove(key)
+                for value in rules:
+                    if key in rules[value]:
+                        rules[value].remove(key)
+    return result
+                
+
+
+
+def part2():
+    # yoink code from part 1
+    f = open("input.txt", "r")
+
+    before = dict() # stores values that need to be before key
+
+    line = f.readline()
+    while(line != "\n"):
+        splitted = line.strip().split("|")
+        first, second = splitted[0], splitted[1]
+        
+        if second in before:
+            before[second].append(first)
+        else:
+            before[second] = [first]
+        line = f.readline()
+
+    updates = f.readlines()
+    result = 0
+
+    for update in updates:
+        correct = True
+        update = update.strip().split(",")
+
+        for i in range(len(update)):
+            if update[i] in before:
+                for value in before[update[i]]:
+                    if value in update[i:]:
+                        correct = False
+
+        if not correct:
+            corrected = make_correct(update, before)
+            result += int(corrected[int(len(corrected)/2)])
+
+    print(result)
+part2()
